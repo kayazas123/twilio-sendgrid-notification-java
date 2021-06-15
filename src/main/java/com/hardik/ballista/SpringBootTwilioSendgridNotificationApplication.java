bc61bfd165432d09a2e3adcca6ae2c962bfe523f
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-15 07:20:59 +0530

    removing spring security default nature

--- src/main/java/com/hardik/ballista/SpringBootTwilioSendgridNotificationApplication.java
+++ src/main/java/com/hardik/ballista/SpringBootTwilioSendgridNotificationApplication.java
@@ -2,8 +2,9 @@ package com.hardik.ballista;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringBootTwilioSendgridNotificationApplication {
 
 	public static void main(String[] args) {
