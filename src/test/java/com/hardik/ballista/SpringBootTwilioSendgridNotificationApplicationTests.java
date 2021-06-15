89ec7f010d300276a599e5aa0b2f254088bbd139
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-15 07:07:29 +0530

    init project commit

---
+++ src/test/java/com/hardik/ballista/SpringBootTwilioSendgridNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.ballista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTwilioSendgridNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
